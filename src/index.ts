0459e4304de63243d362e6eab45c40dca69b4dfd
Ubuntu <ubuntu@ip-172-31-89-242.ec2.internal>
2021-12-06 02:12:59 +0000

    linted

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,27 @@
 * @since   2021-12-05
 */
 
-import * as readline from 'readline';
+import * as readline from 'readline'
 
-function textReverser(s: string) {
-    // this function is a text reverser function.
-    if (s.length == 0)
-    {
-        return s;
-    }
-    else
-    {
-        var lastChar: string = s.substr(s.length - 1);
-        var restOfStr: string = s.substr(0, s.length - 1);
-        var returnStr: string = lastChar + textReverser(restOfStr);
-        return returnStr;
-    }
+function textReverser (s: string) {
+  // this function is a text reverser function.
+  if (s.length === 0) {
+    return s
+  } else {
+    const lastChar: string = s.substr(s.length - 1)
+    const restOfStr: string = s.substr(0, s.length - 1)
+    const returnStr: string = lastChar + textReverser(restOfStr)
+    return returnStr
+  }
 }
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-rl.question("Input a text to reverse: ", function(answer) {
-    var text: string = textReverser(answer);
-    console.log("The reverse of " + answer + " is " + text);
-    rl.close();
-});
+rl.question('Input a text to reverse: ', function (answer) {
+  const text: string = textReverser(answer)
+  console.log('The reverse of ' + answer + ' is ' + text)
+  rl.close()
+})
